fdbda70cc88023e4209ca9c27150800fc9f28cb2
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2018-11-10 19:34:08 +0700

    login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Muhamad Ilham Arrouf', email: 'ilham.arrouf@gmail.com', nickname: 'ilhamarrouf', password: "secret21")
